19f10370e939f1d80221de0eb6f8441d5db0491e
ox <agula002@ucr.edu>
2020-05-26 02:23:49 -0700

    done except for formatting

--- cache.cpp
+++ cache.cpp
@@ -3,47 +3,16 @@
 #include <queue>
 #include <fstream>
 #include <cmath>
+#include <utility>
 
 using namespace std;
 
 #define  BLOCK_SIZE     16
 
-void direct_fifo(int CACHE_SIZE){
-    
-    // initialize file
-    std::ifstream inFS;
-    inFS.open("trace");
-    int number_of_blocks = CACHE_SIZE / BLOCK_SIZE;
-    std::vector<unsigned long long> cache;
-
-    // initialize blocks
-    for (int i = 0; i < number_of_blocks; ++i)
-        cache.push_back(0);
-
-   // read memory accesses from standard input
-    unsigned long long read_line;
-    int total = 0, miss = 0;
-    while (inFS >> std::hex >> read_line) {
-        total++;
-        unsigned long long tag   = read_line >> (10 + 4); // get tag
-        unsigned long long index = read_line >> 4;        // get index
-        index = index & 0x00000000000003FF;
-
-      // check for miss
-        if(cache[index] != tag) {
-            cache[index] = tag;
-            miss++;
-        }
-   }
-    double miss_rate = (double)miss / (double)(total);
-
-    printf("total accesses: %d\n", total);
-    printf("miss rate:      %0.2lf\n", miss_rate * 100.0);
-}
 void n2_fifo(int CACHE_SIZE, int associativity){
-    std::ifstream inFS("trace");
+    //std::ifstream inFS("trace");
     int number_of_blocks = CACHE_SIZE / (BLOCK_SIZE * associativity);
-    std::cout << log2(number_of_blocks) << "\n";
+    //std::cout << log2(number_of_blocks) << "\n";
     int index_bitmask = pow(2,log2(number_of_blocks) ) - 1;
 //    std::cout << std::hex << index_bitmask << "\n" ;
     std::vector<std::deque<unsigned long long> > cache;
@@ -57,7 +26,7 @@ void n2_fifo(int CACHE_SIZE, int associativity){
    // read memory accesses from standard input
     unsigned long long read_line;
     int total = 0, miss = 0;
-    while (inFS >> std::hex >> read_line) {
+    while (cin >> std::hex >> read_line) {
         total++;
         int tag   = read_line >> ((10 - (associativity / 2)) + 4); // get tag
         int index = read_line >> 4;        // get index
@@ -80,21 +49,78 @@ void n2_fifo(int CACHE_SIZE, int associativity){
    }
     double miss_rate = (double)miss / (double)(total);
 
-    printf("total accesses: %d\n", total);
+    //printf("total accesses: %d\n", total);
     printf("miss rate:      %0.2lf\n", miss_rate * 100.0);
-    inFS.close();
+    //inFS.close();
 }
 
-void 
+void lru(int CACHE_SIZE, int associativity) {
+    std::ifstream inFS("trace");
+    int number_of_blocks = CACHE_SIZE / (BLOCK_SIZE * associativity);
+    //std::cout << log2(number_of_blocks) << "\n";
+    int index_bitmask = pow(2,log2(number_of_blocks) ) - 1;
+//    std::cout << std::hex << index_bitmask << "\n" ;
+    std::vector <std::vector<pair<unsigned long long,int>>> cache;
+
+    for (int i = 0; i < number_of_blocks; ++i){
+        std::vector < pair<unsigned long long,int> > block;
+        for(int j= 0 ; j < associativity; j++) block.push_back(make_pair((unsigned long long)0,j));
+        cache.push_back(block);
+    }
+
+   // read memory accesses from standard input
+    unsigned long long read_line;
+    int total = 0, miss = 0;
+    while (cin >> std::hex >> read_line) {
+        total++;
+        int tag   = read_line >> ((10 - (associativity / 2)) + 4); // get tag
+        int index = read_line >> 4;        // get index
+        //int offset = read_line & 0x1;
+        index = index & index_bitmask;
+
+      // check for miss
+      bool missBool = true;
+        for(int k = 0; k < associativity; k++){
+            if(cache[index][k].first == tag){
+                missBool = false;
+                cache[index][k].second = 0;
+            }
+            else cache[index][k].second++;
+            
+        }
+      
+
+        if(missBool){
+            miss++;
+            int maxIndex = 0;
+            for( int  k = 1; k < associativity;k++){
+                if(cache[index][k].second > cache[index][maxIndex].second) maxIndex = k;
+            }
+            cache[index][maxIndex] = make_pair(tag,0);
+        }
+
+   }
+    double miss_rate = (double)miss / (double)(total);
+
+    //printf("total accesses: %d\n", total);
+    printf("miss rate:      %0.2lf\n", miss_rate * 100.0);
+    //inFS.close();
+
+
+}
 
 
 int main () {
    // build cache
-    direct_fifo(16384);
+    //direct_fifo(16384);
     n2_fifo(16384,1);
     n2_fifo(16384,2);
     n2_fifo(16384,4);
     n2_fifo(16384,8);
+    lru(16384,1);
+    lru(16384,2);
+    lru(16384,4);
+    lru(16384,8);
 
    return 0;
 }
